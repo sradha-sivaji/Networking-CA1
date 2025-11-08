421c4ea1267427318b90ec89bf1ce75a220efe16
sradha-sivaji <sradhasivaji22@gmail.com>
2025-11-08 23:53:13 +0000

    Initial1

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "akhil-networking-ca"
+    bucket         = "sradha-networking-ca1"
     key            = "terraform/terraform.tfstate"
     region         = "eu-central-1"
     encrypt        = true
